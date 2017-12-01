ff9e13a838e116c3763628a7b2bfeacf380ab6b4
Israel Davila <idavilam1100@alumno.ipn.mx>
2017-12-01 02:41:00 -0600

    Tune delay values

--- source/main.c
+++ source/main.c
@@ -7,13 +7,13 @@ int main(){
     uart2_init(9600);
     while(1){
         serial_printf(uart2_puts, "Hello, world!\n");
-        delay_ms(500);
+        delay_ms(1000);
     }
 }
 
 void delay_ms(int delay_time){
     for(int i = 0; i < delay_time; i++){
-        for(int j = 0; j < 1000; j++){
+        for(int j = 0; j < 4000; j++){
         }
     }
 }
